d6b417d25273faf3909e7b72060b12dbe5a90a01
harilalajoserak <harilalajoserak@gmail.com>
2025-12-31 21:32:30 +0300

    Refactor token management and enhance start command
    Updated token handling to use environment variables and improved message formatting.

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const TOKEN = "7953446954:AAHiSx2wD6GapU5ccthbEsC-l8pryvM703c";
-const ADMIN_ID = 8516247472;
+// Aza apetraka ato intsony ny token raha hampiasa Railway Variables ianao
+const TOKEN = process.env.TELEGRAM_TOKEN || "8385187452:AAH4etR9nueIOAwc_7kOSkLiM4iIVGpO6C8";
+const ADMIN_ID = Number(process.env.ADMIN_ID) || 8516247472;
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.onText(/\/start (.+)/, (msg, match) => {
+console.log("✅ Bot started...");
+
+// /start tsotra (tsy mila parametre)
+bot.onText(/\/start$/, (msg) => {
   const chatId = msg.chat.id;
-  const data = match[1]; // ex: MAC_1 na MAC_3
 
   if (chatId !== ADMIN_ID) {
-    bot.sendMessage(chatId,
-      "✅ Demande envoyee.\n⏳ Attendez la validation de l administrateur."
+    bot.sendMessage(
+      chatId,
+      "✅ Demande envoyée.\n⏳ Attendez la validation de l’administrateur."
     );
 
-    bot.sendMessage(ADMIN_ID,
-      `📡 DEMANDE WIFI\nUser: ${chatId}\nInfo: ${data}`
-    );
+    bot.sendMessage(ADMIN_ID, `📡 DEMANDE WIFI\n👤 User: ${chatId}`);
   } else {
-    bot.sendMessage(chatId, "Admin connecte");
+    bot.sendMessage(chatId, "👑 Admin connecté.");
   }
-});
+});fix start command
